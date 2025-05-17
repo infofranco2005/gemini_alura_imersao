b7152625abc5bff5bafbd0e2618d1e1e670288da
Cristiano Franco <cristiano465@hotmail.com>
2025-05-17 01:26:58 -0300

    projeto alura

--- chat.py
+++ chat.py
@@ -7,27 +7,28 @@ load_dotenv() #carrega as variaveis do arquivo .env
 apikey = os.getenv("APIKEY")
 client = genai.Client(api_key=apikey)
 chat = client.chats.create(model="gemini-2.0-flash")
-# instrucao = input("Coloque a sua ideia neste campo que resolvemos para voce ")
-# print(instrucao)
-# response = client.models.generate_content(
-#     model="gemini-2.0-flash",
-#     config=types.GenerateContentConfig(
-#         system_instruction=instrucao),
-#     contents="Hello there"
-# )
 
-# print(response.text)
+instrucao = input("Olá fala ai mano :) ")
+print(instrucao)
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    config=types.GenerateContentConfig(
+        system_instruction=instrucao),
+    contents="Hello there"
+)
 
-instrucao1 = input("Coloque a sua ideia neste campo que resolvemos para voce ")
-response = chat.send_message_stream(instrucao1)
+
+
+# instrucao1 = input("Coloque a sua ideia neste campo que resolvemos para voce ")
+response = chat.send_message_stream(instrucao)
 for chunk in response:
-    print(chunk.text, end="")
+     print(chunk.text, end="sim")
 
-instrucao2 = input("Coloque a sua ideia neste campo que resolvemos para voce ")
-response = chat.send_message_stream(instrucao2)
-for chunk in response:
-    print(chunk.text, end="")
+# instrucao2 = input("Coloque a sua ideia neste campo que resolvemos para voce ")
+# response = chat.send_message_stream(instrucao2)
+# for chunk in response:
+#      print(chunk.text, end="")
 
-for message in chat.get_history():
-    print(f'role - {message.role}', end=": ")
-    print(message.parts[0].text)
+# for message in chat.get_history():
+#      print(f'role - {message.role}', end=": ")
+#      print(message.parts[0].text)
